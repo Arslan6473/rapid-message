32240af90e515102cde59ae5b4bbae2eb43b51d2
Arslan Sarwar <arslansarwarwebdev@mail.com>
2024-07-02 20:13:02 -1200

    add middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token")?.value;
+  const token = request.cookies.get("__Secure-next-auth.session-token")?.value;
   const url = request.nextUrl;
 
   // Redirect authenticated users away from signin, signup, verify, and root paths
